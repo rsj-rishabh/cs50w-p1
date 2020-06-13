b3fd70512ecb7af20448e3209fac54da3c28586c
rsj-rishabh <rsj.rishabh@gmail.com>
2020-06-14 00:56:55 +0530

    Registration feature complete.

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
+# for getting path variables
 import os
 
+# for flask operations
 from flask import Flask, session, render_template, request
 from flask_session import Session
+
+# for database operations
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_sqlalchemy import SQLAlchemy
@@ -13,19 +17,16 @@ app = Flask(__name__)
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
 # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
+engine = create_engine(os.getenv("DATABASE_URL"))
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db.init_app(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', message='')
 
 @app.route("/register")
 def go_to_register():
@@ -38,6 +39,10 @@ def register():
     name = request.form.get('name')
     passcode = request.form.get('passcode')
     user = User(id=id, name=name, passcode=passcode)
-    
-    db.session.add(user)
-    return render_template('index.html')
+    try:
+        db.session.add(user)
+        print(f'User {id} added to the database.')
+        db.session.commit()
+        return render_template('index.html', message=f'Welcome {name} to the Book-o-holics fam!')
+    except:
+        return render_template('index.html', message=f'Alas! The user with ID {id} already exists.')
