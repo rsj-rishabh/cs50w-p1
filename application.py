fd972dbd4eeff81c6ebd82cb155c9238a21e6fc8
rsj-rishabh <rsj.rishabh@gmail.com>
2020-06-13 13:42:31 +0530

    Testing git using Atom

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
 
@@ -17,9 +19,9 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
+db.init_app(app)
 
 @app.route("/")
 def index():
@@ -32,4 +34,10 @@ def go_to_register():
 
 @app.route("/registration_status", methods=['POST'])
 def register():
+    id = request.form.get('user_id')
+    name = request.form.get('name')
+    passcode = request.form.get('passcode')
+    user = User(id=id, name=name, passcode=passcode)
+    
+    db.session.add(user)
     return render_template('index.html')
